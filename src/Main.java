db4b39a56ee7fae3e7d6e814f231c7152bc3739b
Hybusa <alkouzne007@gmail.com>
2023-01-13 20:09:14 +0300

    Block completion (major struct changes)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+    }
 
+    public static void task1() {
 
         //First task
 
@@ -11,19 +18,20 @@ public class Main {
 
         System.out.println("Задание 1.");
 
-        int a1 = 1000;
-        int q1 = 5;
-        while (q1 != 0)
-        {
-            if (a1%4==0)
-            {
-                System.out.println(a1);
-                q1--;
+        int a = 1000;
+        int q = 5;
+        while (q != 0) {
+            if (a % 4 == 0) {
+                System.out.println(a);
+                q--;
             }
-            a1++;
+            a++;
         }
         System.out.println(System.lineSeparator());
 
+    }
+
+    public static void task2() {
         //Задание 2
         /*
         Напишите программу, которая выводит в консоль первые 10 элементов последовательности 1 3 5 7 9...
@@ -31,16 +39,17 @@ public class Main {
 
         System.out.println("Задание 2.");
 
-        int a2 = 1;
-        for (int i2=0; i2<10; i2++)
-        {
-            System.out.println(a2);
-            a2+=2;
+        int a = 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a);
+            a += 2;
         }
 
         System.out.println(System.lineSeparator());
 
+    }
 
+    public static void task3() {
 
         //Задание 3
         /*
@@ -49,16 +58,17 @@ public class Main {
         */
         System.out.println("Задание 3.");
 
-        int a3 = 90;
-        while (a3 >= 0)
-        {
-            System.out.println(a3);
-            a3-=5;
+        int a = 90;
+        while (a >= 0) {
+            System.out.println(a);
+            a -= 5;
         }
 
         System.out.println(System.lineSeparator());
+    }
 
 
+    public static void task4() {
         //Задание 4
         /*
          Напишите программу, которая выводит в консоль первые 10 элементов
@@ -66,16 +76,16 @@ public class Main {
         */
         System.out.println("Задание 4.");
 
-        int a4 = 2;
-        for (int i=0; i<10; i++)
-        {
-            System.out.println(a4);
-            a4*=2;
+        int a = 2;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a);
+            a *= 2;
         }
 
         System.out.println(System.lineSeparator());
+    }
 
-
+    public static void task5() {
         //Задание 5
         /*
          Напишите программу, которая выводит даты всех выходных дней месяца при условии,
@@ -83,17 +93,18 @@ public class Main {
         */
         System.out.println("Задание 4.");
 
-        for (int i = 1; i <= 31; i++)
-        {
+        for (int i = 1; i <= 31; i++) {
             System.out.println(i);
             i++;
             System.out.println(i);
-            i+=5;
+            i += 5;
         }
 
         System.out.println(System.lineSeparator());
 
+    }
 
+    public static void task6() {
         //Задание 6
         /*
         В городе Туле проезд в трамвае осуществляется по бумажным отрывным билетам.
@@ -105,22 +116,23 @@ public class Main {
 
         System.out.println("Задание 5.");
 
-        int amount =0;
-        for (int i = 1; i <= 999999;i++)
-        {
-          int firstP6 = i/1000;
-          int secondP6 = i%1000;
+        int amount = 0;
+        for (int i = 1; i <= 999999; i++) {
+            int firstP6 = i / 1000;
+            int secondP6 = i % 1000;
 
-          if(calc(firstP6) == calc(secondP6))
-              amount++;
+            if (calc(firstP6) == calc(secondP6))
+                amount++;
 
-              // System.out.println(i);
+            // System.out.println(i);
 
         }
 
         System.out.println(amount);
         System.out.println(System.lineSeparator());
     }
+
+
     private static int calc (int numb)
     {
         int result;
